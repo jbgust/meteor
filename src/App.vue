e4df5f9fa478dbb4c1ae115cc88e104f56dcf184
jordan38 <jordan.content@outlook.fr>
2019-01-30 14:09:34 +0100

    WIP: Initialize Motor parameter form

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
     <b-container>
-        toto
+      <solid-rocket-motor></solid-rocket-motor>
     </b-container>
 </template>
 
 <script>
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import SolidRocketMotor from './components/SolidRocketMotor'
+
+Vue.use(BootstrapVue)
 
 Vue.use(BootstrapVue)
 
 export default {
   name: 'app',
-  components: {}
+  components: { SolidRocketMotor }
 }
 </script>
 
---
+++ src/components/SolidRocketMotor.vue
@@ -0,0 +1,71 @@
+<template>
+  <b-form>
+    <b-form-group label="Throat diameter [mm]:">
+      <b-form-input id="throatDiameter"/>
+    </b-form-group>
+    <b-form-group label="Grain outer diameter [mm]:">
+      <b-form-input id="outerDiameter"/>
+    </b-form-group>
+    <b-form-group label="Grain core diameter [mm]:">
+      <b-form-input id="coreDiameter"/>
+    </b-form-group>
+    <b-form-group label="Grain segment length [mm]:">
+      <b-form-input id="segmentLength"/>
+    </b-form-group>
+    <b-form-group label="Number of segment:">
+      <b-form-input id="numberOfSegment"/>
+    </b-form-group>
+
+    <b-form-group label="Outer surface:">
+      <b-form-select id="outerSurface" name="outerSurface" :options="grainSurfaces" />
+    </b-form-group>
+    <b-form-group label="Ends surface:">
+      <b-form-select id="endsSurface" :options="grainSurfaces" />
+    </b-form-group>
+
+    <b-form-group label="Core surface:">
+      <b-form-select id="coreSurface" :options="grainSurfaces" />
+    </b-form-group>
+
+    <b-form-group label="Propellant:">
+      <b-form-select id="propellantType" :options="propellantType" />
+    </b-form-group>
+
+    <b-form-group label="Chamber inner diameter [mm]:">
+      <b-form-input id="chamberInnerDiameter"/>
+    </b-form-group>
+    <b-form-group label="Chamber length [mm]:">
+      <b-form-input id="chamberLength"/>
+    </b-form-group>
+
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
+  </b-form>
+</template>
+
+<script>
+
+export default {
+  name: 'solid-rocket-motor',
+  data () {
+    return {
+      grainSurfaces: [
+        { text: 'Exposed', value: 'EXPOSED' },
+        { text: 'Inhibited', value: 'INHIBITED' }
+      ],
+      propellantType: [
+        { text: 'KNDX', value: 'KNDX' },
+        { text: 'KNER_COARSE', value: 'KNER_COARSE' },
+        { text: 'KNMN_COARSE', value: 'KNMN_COARSE' },
+        { text: 'KNSB_COARSE', value: 'KNSB_COARSE' },
+        { text: 'KNSB_FINE', value: 'KNSB_FINE' },
+        { text: 'KNSU', value: 'KNSU' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
