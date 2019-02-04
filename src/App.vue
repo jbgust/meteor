6009bb0f8d888ca89d2983b8a60bfee95f49da91
jbise <jbise@tennaxia.com>
2019-02-04 13:52:54 +0100

    Adding motor caracteristic result

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
       <b-row>
         <b-col lg="4">
           <solid-rocket-motor @computation-success="loadResult"></solid-rocket-motor>
+          <performance-info ref="performanceResult"></performance-info>
         </b-col>
         <b-col>
           <thrust-graphical-result ref="thrustGraphicalResult"/>
@@ -16,15 +17,17 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import SolidRocketMotor from './components/SolidRocketMotor'
 import ThrustGraphicalResult from './components/ThrustGraphicalResult'
+import PerformanceInfo from "./components/PerformanceInfo";
 
 Vue.use(BootstrapVue)
 
 export default {
   name: 'app',
-  components: { ThrustGraphicalResult, SolidRocketMotor },
+  components: { PerformanceInfo, ThrustGraphicalResult, SolidRocketMotor },
   methods: {
     loadResult (data) {
       this.$refs.thrustGraphicalResult.chart.data = data.thrustResults
+      this.$refs.performanceResult.performance = data.performanceResult
     }
   }
 }
---
+++ src/components/PerformanceInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <b-container>
+        <h3 class="title-result">Performance</h3>
+        <div class="resultat"><span class="label-resultat">Class : </span><span v-text="performance.motorDescription"></span></div>
+        <div class="resultat"><span class="label-resultat">Thrust time : </span><span v-text="performance.thrustTime"></span></div>
+        <div class="resultat"><span class="label-resultat">Max thrust : </span><span v-text="performance.maxThrust"></span></div>
+        <div class="resultat"><span class="label-resultat">Total impulse : </span><span v-text="performance.totalImpulse"></span></div>
+        <div class="resultat"><span class="label-resultat">Specific impulse : </span><span v-text="performance.specificImpulse"></span></div>
+        <div class="resultat"><span class="label-resultat">Max pressure : </span><span v-text="performance.maxPressure"></span></div>
+
+        <h3 class="title-result">Nozzle</h3>
+        <div class="resultat"><span class="label-resultat" style="color: orange;" v-if="performance.optimalDesign">Optimal design</span></div>
+        <div class="resultat"><span class="label-resultat">Nozzle exit diam. : </span><span v-text="performance.nozzleExitDiameter"></span></div>
+        <div class="resultat"><span class="label-resultat">Divergence length : </span><span v-text="performance.divergenceLenght"></span></div>
+        <div class="resultat"><span class="label-resultat">Convergence length : </span><span v-text="performance.convergenceLenght"></span></div>
+        <div class="resultat"><span class="label-resultat">Initial exit speed : </span><span v-text="performance.exitSpeedInitial"></span></div>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "PerformanceInfo",
+      data() {
+        return {
+          performance: {}
+        }
+      },
+    }
+</script>
+
+<style scoped>
+  .title-result {
+    margin-bottom:5px;
+  }
+  .resultat {
+
+  }
+  .label-resultat {
+    font-weight: bold;
+    display: inline-block;
+    width: 185px;
+    padding-left: 20px;
+    margin: 0 10px;
+    text-align: right;
+  }
+</style>
