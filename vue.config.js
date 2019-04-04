6291a4806cbbcf2225556e29aafe958e8d295d17
jbgust <jeromebise@hotmail.com>
2019-04-04 13:17:33 +0200

    Display browser is not supported for IE11

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const ObsoleteWebpackPlugin = require('obsolete-webpack-plugin');
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new ObsoleteWebpackPlugin({
+                promptOnNonTargetBrowser: true,
+                template: '<div style="border-radius: 15px;font-family: Roboto sans-serif;background-color: #3f51b5;color: white;padding:30px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);"><h1>Your browser is not supported by METEOR, please update your browser.</h1></div>'
+            })
+        ]
+    }
+}
