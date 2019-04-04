f946868d3ebb0873d0b92d8a61312f08b9a18c0c
Jerome Bise <jeromebise@hotmail.com>
2019-04-04 22:10:59 +0200

    Adding pre-compression

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const ObsoleteWebpackPlugin = require('obsolete-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     configureWebpack: {
@@ -6,6 +7,9 @@ module.exports = {
             new ObsoleteWebpackPlugin({
                 promptOnNonTargetBrowser: true,
                 template: '<div style="border-radius: 15px;font-family: Roboto sans-serif;background-color: #3f51b5;color: white;padding:30px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);"><h1>Your browser is not supported by METEOR, please update your browser.</h1></div>'
+            }),
+            new CompressionPlugin({
+                cache: true
             })
         ]
     }
