6b2170d27568a4717368ef7745c26c0f58f47eb2
Jerome Bise <j.bise@sheldon-skills.com>
2023-05-27 18:10:12 +0200

    Add new SRM propellants

--- src/modules/grainsConstants.mjs
+++ src/modules/grainsConstants.mjs
@@ -19,15 +19,19 @@ export const KNSB_COARSE = 'KNSB_COARSE'
 export const KNSB_FINE = 'KNSB_FINE'
 export const KNSU = 'KNSU'
 export const KNXY = 'KNXY'
+export const KNFR = 'KNFR'
+export const KNPSB = 'KNPSB'
 
 export const CUSTOM_PROPELLANT_PREFIX = 'CUSTOM_'
 
 export const NATIVE_PROPELLANTS = [
+    { value: KNSB_FINE, text: 'Sorbitol (KNSB - oxidizer finely milled)', description: '65% KNO3 / 35% sorbitol', idealDensity: '1.841 g/cm³' },
+    { value: KNSB_COARSE, text: 'Sorbitol (KNSB - oxidizer lightly milled)', description: '65% KNO3 / 35% sorbitol', idealDensity: '1.841 g/cm³' },
+    { value: KNSU, text: 'Sucrose (KNSU)', description: '65% KNO3 / 35% sucrose', idealDensity: '1.889 g/cm³' },
     { value: KNDX, text: 'Dextrose (KNDX)', description: '65% KNO3 / 35% Dextrose', idealDensity: '1.879 g/cm³' },
     { value: KNER_COARSE, text: 'Erythritol (KNER - oxidizer lightly milled)', description: '65% KNO3 / 35% erythritol', idealDensity: '1.820 g/cm³' },
     { value: KNMN_COARSE, text: 'Mannitol (KNMN - oxidizer lightly milled)', description: '65% KNO3 / 35% mannitol', idealDensity: '1.854 g/cm³' },
-    { value: KNSB_COARSE, text: 'Sorbitol (KNSB - oxidizer lightly milled)', description: '65% KNO3 / 35% sorbitol', idealDensity: '1.841 g/cm³' },
-    { value: KNSB_FINE, text: 'Sorbitol (KNSB - oxidizer finely milled)', description: '65% KNO3 / 35% sorbitol', idealDensity: '1.841 g/cm³' },
-    { value: KNSU, text: 'Sucrose (KNSU)', description: '65% KNO3 / 35% sucrose', idealDensity: '1.889 g/cm³' },
-    { value: KNXY, text: 'Xylitol (KNXY)', description: '65% KNO3 / 35% xylitol', idealDensity: '1.8654 g/cm³' }
+    { value: KNXY, text: 'Xylitol (KNXY)', description: '65% KNO3 / 35% xylitol', idealDensity: '1.8654 g/cm³' },
+    { value: KNFR, text: 'Fructose (KNFR)', description: '65% KNO3 / 35% fructose', idealDensity: '1.942 g/cm³' },
+    { value: KNPSB, text: 'Potas. nitrate+perchlorate (KNPSB)', description: '35% KNO3 / 30% KClO4 / 35% sorbitol', idealDensity: '1.923 g/cm³' }
 ]
